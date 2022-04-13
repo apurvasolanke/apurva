e6135ced0d01ad76febc58d7c8c6a05794fd2616
apurva solanke <APURVA.RAOSAHEB-SOLANKE@CAPGEMINI.COM>
2022-04-13 19:20:27 +0530

    Provide commit

---
+++ src/test/java/Boot/SpringBoot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Boot.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
