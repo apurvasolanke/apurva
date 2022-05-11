fe8517770ece8d923bb0b7e99081d71ef3623a92
apurva solanke <APURVA.RAOSAHEB-SOLANKE@CAPGEMINI.COM>
2022-05-11 10:06:23 +0530

    folder

--- flight-booking-frontend/src/pages/Contactus.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function Contactus(){
-    return(
-        <div className="m-3">
-            <h3>Contact Us</h3>
-        </div>
-    );
-}
--- flight-booking-frontend/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers, createStore } from 'redux'
-import reducer from './reducer'
-
-//const store=createStore(reducer)
-//export default store;
-
-const combreducer=combineReducers({loggedin:reducer});
-const store=createStore(combreducer)
-export default store;
